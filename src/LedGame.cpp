0eb1b0b437f731bf5fb916093029e00bc47a4cee
ppolakowski <p.polakowski@proton.me>
2024-06-11 12:04:44 +0200

    Fixed build error

--- src/LedGame.cpp
+++ src/LedGame.cpp
@@ -2,32 +2,6 @@
 #include "../lib/defines.h"
 #include "../lib/EEPROMController.h"
 
-uint16_t LedGame::play() {
-    unsigned long previousMillis = millis();
-
-    unsigned long interval = beginGame() + previousMillis;
-
-    while (true) {
-        if (millis() > interval) {
-            break;
-        } else {
-            if (!digitalRead(BUTTON_ENTER_PIN) == HIGH) {
-                return 1;
-            }
-        }
-    }
-
-    startTime = millis();
-
-    while (!digitalRead(BUTTON_ENTER_PIN) == LOW) {
-        turnOnDevice();
-    }
-
-    stopTime = millis();
-    turnOffDevice();
-    return stopTime - startTime;
-
-}
 
 
 void LedGame::turnOnDevice() {
--- src/SoundGame.cpp
+++ src/SoundGame.cpp
@@ -1,31 +1,6 @@
 #include "Arduino.h"
 #include "../lib/SoundGame.h"
 
-uint16_t SoundGame::play() {
-    unsigned long previousMillis = millis();
-
-    unsigned long interval = beginGame() + previousMillis;
-
-    while (true) {
-        if (millis() > interval) {
-            break;
-        } else {
-            if (!digitalRead(BUTTON_ENTER_PIN) == HIGH) {
-                return 1;
-            }
-        }
-    }
-
-    startTime = millis();
-
-    //play sound
-    while (!digitalRead(BUTTON_ENTER_PIN) == LOW) {
-        turnOnDevice();
-    }
-    stopTime = millis();
-    turnOffDevice();
-    return stopTime - startTime;
-}
 
 void SoundGame::turnOnDevice() {
     digitalWrite(SPEAKER_PIN, HIGH);
